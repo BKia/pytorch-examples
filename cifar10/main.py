ec8d177d5b212fc86f382db89cfc70bf7bd69dd5
wenwei202 <weiwen.web@gmail.com>
2019-01-17 09:34:08 -0800

    reorganize code

--- cifar10/main.py
+++ cifar10/main.py
@@ -14,78 +14,40 @@ import os
 import argparse
 
 from models import *
-from utils import progress_bar
-
+import logging
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--epochs', default=300, type=int, help='the number of epochs')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+save_path = os.path.join('./results', datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+else:
+    raise OSError('Directory {%s} exists. Use a new one.' % save_path)
+logging.basicConfig(filename=os.path.join(save_path, 'log.txt'), level=logging.INFO)
+logger = logging.getLogger('main')
+logger.addHandler(logging.StreamHandler())
+logger.info("Saving to %s", save_path)
+logger.info("Running arguments: %s", args)
 best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-# Model
-print('==> Building model..')
-# net = VGG('VGG19')
-net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
-
-if args.resume:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.t7')
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+def decay_learning_rate(optimizer):
+    """Sets the learning rate to the initial LR decayed by 10"""
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = param_group['lr'] * 0.1
 
 # Training
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
+def train(epoch, net, loader, optimizer, criterion, device='cuda'):
+    logger.info('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
     correct = 0
     total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
+    for batch_idx, (inputs, targets) in enumerate(loader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
@@ -98,17 +60,20 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        if 0 == batch_idx % 100 or batch_idx == len(loader) - 1:
+            logger.info('(%d/%d) ==> Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                        % (
+                        batch_idx + 1, len(loader), train_loss / (batch_idx + 1), 100. * correct / total, correct,
+                        total))
 
-def test(epoch):
+def test(epoch, net, loader, criterion, device='cuda'):
     global best_acc
     net.eval()
     test_loss = 0
     correct = 0
     total = 0
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
+        for batch_idx, (inputs, targets) in enumerate(loader):
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = net(inputs)
             loss = criterion(outputs, targets)
@@ -118,13 +83,14 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            if 0 == batch_idx % 100 or batch_idx == len(loader) - 1:
+                logger.info('(%d/%d) ==> Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                    % (batch_idx+1, len(loader), test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
     if acc > best_acc:
-        print('Saving..')
+        logger.info('Saving..')
         state = {
             'net': net.state_dict(),
             'acc': acc,
@@ -136,6 +102,70 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+    # Data
+    logger.info('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # Model
+    logger.info('==> Building model..')
+    # net = VGG('VGG19')
+    net = ResNet18()
+    # net = PreActResNet18()
+    # net = GoogLeNet()
+    # net = DenseNet121()
+    # net = ResNeXt29_2x64d()
+    # net = MobileNet()
+    # net = MobileNetV2()
+    # net = DPN92()
+    # net = ShuffleNetG2()
+    # net = SENet18()
+    net = net.to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+
+    if args.resume:
+        # Load checkpoint.
+        logger.info('==> Resuming from checkpoint..')
+        assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+        checkpoint = torch.load('./checkpoint/ckpt.t7')
+        net.load_state_dict(checkpoint['net'])
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+
+    for epoch in range(start_epoch, start_epoch+args.epochs):
+        train(epoch, net, trainloader, optimizer, criterion)
+        test(epoch, net, testloader, criterion)
+        if (epoch + 1) % (args.epochs/3) == 0:
+            logger.info('======> decaying learning rate')
+            decay_learning_rate(optimizer)
+
+
+if __name__ == '__main__':
+    main()
--- cifar10/utils.py
+++ cifar10/utils.py
@@ -7,118 +7,92 @@ import os
 import sys
 import time
 import math
+import torch
+import copy
 
 import torch.nn as nn
 import torch.nn.init as init
 
+class MovingMaximum(object):
+    def __init__(self):
+        self.data = [] # data[i] is the maximum val in data[0:i+1]
+        self.max = 0.0
 
-def get_mean_and_std(dataset):
-    '''Compute the mean and std value of dataset.'''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('==> Computing mean and std..')
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
+    def push(self, current_data):
+        if len(self.data) == 0:
+            self.max = current_data
+        elif current_data > self.max:
+            self.max = current_data
+        self.data.append(self.max)
 
-def init_params(net):
-    '''Init layer parameters.'''
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            init.constant(m.weight, 1)
-            init.constant(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
+    def get(self):
+        return self.data
+
+    def delta(self, start, end):
+        try:
+            res = self.data[end] - self.data[start]
+        except IndexError:
+            res = self.data[end]
+        return res
+
+class ExponentialMovingAverage(object):
+    def __init__(self, decay=0.95):
+        self.data = []
+        self.decay = decay
+        self.avg_val = 0.0
+
+    def push(self, current_data):
+        self.avg_val = self.decay * self.avg_val + (1 - self.decay) * current_data
+        self.data.append(self.avg_val)
+
+    def get(self):
+        return self.data
+
+    def delta(self, start, end):
+        try:
+            res = self.data[end] - self.data[start]
+        except IndexError:
+            res = self.data[end]
+        return res
+
+class TorchExponentialMovingAverage(object):
+    def __init__(self, decay=0.999):
+        self.decay = decay
+        self.ema = {}
+        self.number = {}
+
+    def push(self, current_data):
+        assert isinstance(current_data, dict), "current_data should be a dict"
+        for key in current_data:
+            if key in self.ema:
+                # in-place
+                self.ema[key] -= (1.0 - self.decay) * (self.ema[key] - current_data[key])
+                self.number[key] += 1
+            else:
+                # self.ema[key] = copy.deepcopy(current_data[key])
+                self.ema[key] = current_data[key] * (1.0 - self.decay)
+                self.number[key] = 1
+
+    def average(self):
+        scaled_ema = {}
+        for key in self.ema:
+            scaled_ema[key] = self.ema[key] / (1.0 - self.decay ** self.number[key])
+        return scaled_ema
 
 
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
+# net: pytorch module
+# strict: strict matching for set
+def set_named_parameters(net, named_params, strict=True):
+    assert isinstance(named_params, dict), "named_params should be a dict"
+    orig_params_data = {}
+    for n, p in net.named_parameters():
+        orig_params_data[n] = copy.deepcopy(p.data)
+    if strict:
+        assert len(named_params) == len(list(net.named_parameters())), "Unmatched number of params!"
+    for n, p in net.named_parameters():
+        if strict:
+            assert n in named_params, "Unknown param name!"
+        if n in named_params:
+            p.data.copy_(named_params[n])
 
-TOTAL_BAR_LENGTH = 65.
-last_time = time.time()
-begin_time = last_time
-def progress_bar(current, total, msg=None):
-    global last_time, begin_time
-    if current == 0:
-        begin_time = time.time()  # Reset for new bar.
-
-    cur_len = int(TOTAL_BAR_LENGTH*current/total)
-    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
-
-    sys.stdout.write(' [')
-    for i in range(cur_len):
-        sys.stdout.write('=')
-    sys.stdout.write('>')
-    for i in range(rest_len):
-        sys.stdout.write('.')
-    sys.stdout.write(']')
-
-    cur_time = time.time()
-    step_time = cur_time - last_time
-    last_time = cur_time
-    tot_time = cur_time - begin_time
-
-    L = []
-    L.append('  Step: %s' % format_time(step_time))
-    L.append(' | Tot: %s' % format_time(tot_time))
-    if msg:
-        L.append(' | ' + msg)
-
-    msg = ''.join(L)
-    sys.stdout.write(msg)
-    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
-        sys.stdout.write(' ')
-
-    # Go back to the center of the bar.
-    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
-        sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current+1, total))
-
-    if current < total-1:
-        sys.stdout.write('\r')
-    else:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
-
-def format_time(seconds):
-    days = int(seconds / 3600/24)
-    seconds = seconds - days*3600*24
-    hours = int(seconds / 3600)
-    seconds = seconds - hours*3600
-    minutes = int(seconds / 60)
-    seconds = seconds - minutes*60
-    secondsf = int(seconds)
-    seconds = seconds - secondsf
-    millis = int(seconds*1000)
-
-    f = ''
-    i = 1
-    if days > 0:
-        f += str(days) + 'D'
-        i += 1
-    if hours > 0 and i <= 2:
-        f += str(hours) + 'h'
-        i += 1
-    if minutes > 0 and i <= 2:
-        f += str(minutes) + 'm'
-        i += 1
-    if secondsf > 0 and i <= 2:
-        f += str(secondsf) + 's'
-        i += 1
-    if millis > 0 and i <= 2:
-        f += str(millis) + 'ms'
-        i += 1
-    if f == '':
-        f = '0ms'
-    return f
+    return orig_params_data
