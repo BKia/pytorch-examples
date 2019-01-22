2cb1da8c22190646fc0c2f54b4eecb979f70f784
wenwei202 <weiwen.web@gmail.com>
2019-01-21 17:56:00 -0800

    revert

--- cifar10/models/resnet.py
+++ cifar10/models/resnet.py
@@ -200,7 +200,7 @@ class CifarBNConvReLUNet(nn.Module):
         super(CifarBNConvReLUNet, self).__init__()
         self.in_planes = 16
 
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU()
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
@@ -222,8 +222,7 @@ class CifarBNConvReLUNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        # out = self.relu(self.bn1(self.conv1(x)))
-        out = self.relu(self.conv1(x))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
